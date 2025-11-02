6f92217ae1ae6248bbeec70b8ea6f7383e2e61cc
kimsz123456 <kimsz123456@gmail.com>
2025-11-02 13:17:19 +0900

    fix: 테스트 수정

--- src/test/java/lotto/WinningLottoTest.java
+++ src/test/java/lotto/WinningLottoTest.java
@@ -1,11 +1,11 @@
 package lotto;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
-
-import static org.assertj.core.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 class WinningLottoTest {
 
@@ -14,7 +14,7 @@ class WinningLottoTest {
     void 당첨로또를_생성한다() {
         Lotto winningLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
         int bonusNumber = 7;
-        
+
         assertThatCode(() -> new WinningLotto(winningLotto, bonusNumber))
                 .doesNotThrowAnyException();
     }
@@ -23,8 +23,9 @@ class WinningLottoTest {
     @Test
     void 보너스_번호_중복_예외() {
         Lotto winningLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
-        
+
         assertThatThrownBy(() -> new WinningLotto(winningLotto, 6))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("중복");
     }
 }
